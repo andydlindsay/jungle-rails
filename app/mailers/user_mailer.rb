622ebafc77a700339fc5ed76b61e3dc88289954d
Andy Lindsay <andydlindsay@gmail.com>
2018-12-06 19:59:09 -0800

    protects admin pages behind password wall

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def order_confirmation(order)
     @order = order[:order]
     @cart = order[:cart]
-    mail(to: "andydlindsay@gmail.com", subject: "Order Confirmation for Order ##{@order.id}")
+    mail(to: @order.email, subject: "Order Confirmation for Order ##{@order.id}")
   end
 
 end
